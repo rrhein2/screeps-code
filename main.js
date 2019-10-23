e5b3ffa13ece7f1a6c3f2b19d6331aabae9c26c4
rrhein2 <richardrhein2@gmail.com>
2019-10-23 13:58:56 -0500

    initial commit.  contains mostly the code I made in the tutorial

---
+++ main.js
@@ -0,0 +1,57 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+    
+    for(var i in Memory.creeps) {
+        if(!Game.creeps[i]) {
+            delete Memory.creeps[i];
+        }
+    }
+
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+    
+    var  harvs = 0, upgrs = 0; buildrs = 0;
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+            harvs = harvs+1;
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+            upgrs = upgrs+1;
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+            buildrs = buildrs+1;
+        }
+    }
+    
+    if(harvs < 2)
+    {
+        Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, CARRY], Game.time, {memory:{role:'harvester'}})
+    }
+    if(upgrs < 2)
+    {
+        Game.spawns['Spawn1'].spawnCreep([MOVE, MOVE, WORK, WORK, CARRY, CARRY], Game.time, {memory:{role:'upgrader'}})
+    }
+    if(buildrs<1)
+    {
+        Game.spawns['Spawn1'].spawnCreep([MOVE, WORK, CARRY], Game.time, {memory:{role:'builder'}})
+    }
+}
---
+++ role.harvester.js
@@ -0,0 +1,52 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.carry.energy < creep.carryCapacity && !creep.memory.full) {
+            var sources = creep.pos.findClosestByRange(FIND_SOURCES);
+            //console.log(sources);
+            //var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+            if(creep.carry.energy == creep.carryCapacity)
+            {
+                creep.memory.full = true;
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+                if(creep.carry.energy == 0)
+                {
+                    creep.memory.full = false;
+                }
+            }
+            // Temporary code to convert to construction worker until I figure out a better way
+            else// if(targets = creep.room.find(FIND_CONSTRUCTION_SITES) && targets.length)
+            {
+                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                if(targets.length) {
+                    if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+                if(creep.carry.energy == 0)
+                {
+                    creep.memory.full = false;
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester;
