989e54f048da02cb7b098f7d941b8b1980f21609
rrhein2 <richardrhein2@gmail.com>
2019-10-24 23:41:05 -0500

    created prototypes for creep to help manage roles better

---
+++ prototype.creep.js
@@ -0,0 +1,30 @@
+var roles = 
+{
+	harvester: require('role.harvester'),
+	//stationedHarvester: require('role.stationedHarvester'),
+	//ferry: require('role.ferry'),
+	builder: require('role.builder'),
+	upgrader: require('role.builder')
+}
+
+Creep.prototype.runRole =
+	function()
+	{
+		if(!this.spawning)
+		{
+			try
+			{
+				roles[this.memory.role].run(this);	
+			} 
+			catch(error)
+			{
+				console.log('Creep: ' + this.name + ' (room: ' + this.room.name + ') throwing role error ' + error);
+			}
+		}
+	};
+
+Creep.prototype.runOtherRole =
+	function(other)
+	{
+		roles[other].run(this);
+	};
