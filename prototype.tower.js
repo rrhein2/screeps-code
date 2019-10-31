f45b33f76ce7f1cac2221332957c2b5abcdd159e
rrhein2 <richardrhein2@gmail.com>
2019-10-30 23:22:40 -0500

    added tower prototype that attacks enemy creeps and then secondarily  heals or repairs

---
+++ prototype.tower.js
@@ -0,0 +1,22 @@
+StructureTower.prototype.runRole = 
+    function() {
+        let closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile != undefined) {
+            this.attack(closestHostile);
+        } else {
+            let closestHurt = this.pos.findClosestByRange(FIND_MY_CREEPS, {
+                filter: c => c.hits < c.hitsMax
+            });
+            if(closestHurt != undefined) {
+                this.heal(closestHurt);
+            }
+
+            let closestDamagedStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART
+                // filter: (structure) => structure.hits < structure.hitsMax// && structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART
+            });
+            if(closestDamagedStructure != undefined) {
+                this.repair(closestDamagedStructure);
+            }
+        }
+    };
