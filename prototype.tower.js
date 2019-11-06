8789ad21b93c82bf6c33280c1a80476e3c671a8b
rrhein2 <richardrhein2@gmail.com>
2019-11-06 01:58:38 -0600

    Modified so it will not repair containers because they are far away

--- prototype.tower.js
+++ prototype.tower.js
@@ -11,12 +11,12 @@ StructureTower.prototype.runRole =
                 this.heal(closestHurt);
             }
 
-            let closestDamagedStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART
-                // filter: (structure) => structure.hits < structure.hitsMax// && structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART
-            });
-            if(closestDamagedStructure != undefined) {
-                this.repair(closestDamagedStructure);
-            }
+            // let closestDamagedStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+            //     filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART
+            //     // filter: (structure) => structure.hits < structure.hitsMax// && structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART
+            // });
+            // if(closestDamagedStructure != undefined) {
+            //     this.repair(closestDamagedStructure);
+            // }
         }
     };
