1c87fd34f9666078171878f63d71c74d8796d5ae
Richie Rhein <richardrhein2@gmail.com>
2019-11-14 11:41:16 -0600

    Implemented movement and exploring.  Fixed bug where it would get stuck in the room border between two rooms

--- role.cartographer.js
+++ role.cartographer.js
@@ -3,28 +3,42 @@ var roleCartographer =
 	run: function(creep)
 	{
 		// Add to spawn queue of home room
-		if(Game.time%30 == 0)
-		{
-			if(creep.ticksToLive < 30)
-			{
-				Game.rooms[creep.memory.home].memory.spawnQueue += ("CT,");
-			}
-		}
-
+		// if(creep.hits < creep.hitsMax)
+		// {
+		// 	console.log("inside suicide pact");
+		// 	Game.rooms[creep.memory.home].memory.spawnQueue += ("CT,");
+		// 	creep.suicide();
+		// }
+		// if(Game.time%30 == 0)
+		// {
+		// 	if(creep.ticksToLive < 30)
+		// 	{
+		// 		Game.rooms[creep.memory.home].memory.spawnQueue += ("CT,");
+		// 	}
+		// }
 		// Randomly choose an adjacent room if you don't have a destination
 		if(creep.memory.destination == undefined || creep.memory.destination == creep.room.name)
 		{
-			var direction = Math.floor(Math.random()*4);
-			creep.memory.destination = (direction == 0 ? "1" : (direction == 1 ? "3" : (direction == 2 ? "5" : (direction == 3 ? "7" : "-1"))));
+			creep.moveTo(25,25);
+			// Memory.rooms[creep.memory.home].area.push(creep.memory.destination);
+			// creep.memory.destination = Memory.rooms[creep.memory.home].area.shift();
+			
+			// var direction = Math.floor(Math.random()*4);
+			// creep.memory.destination = (direction == 0 ? "1" : (direction == 1 ? "3" : (direction == 2 ? "5" : (direction == 3 ? "7" : "-1"))));
+			// creep.memory.destination = Game.map.describeExits(creep.room.name)[creep.memory.destination];
 		}
-		if(creep.room.memory.searchTime == undefined || creep.room.memory.searchTime <= Game.time)
+		if(creep.room.memory.searchTime == undefined || (creep.room.memory.searchTime <= Game.time && creep.room.memory.searchTime != -1))
 		{
+			console.log('here');
 			creep.room.explore();
 		}
 		else
 		{
-			creep.travelTo(creep.pos.findClosestByPath(creep.memory.destination));
+			creep.moveTo(creep.pos.findClosestByPath(creep.room.findExitTo(creep.memory.destination)));
+			//console.log(creep.room.findExitTo(creep.memory.destination));
 		}
 
 	}
 }
+
+module.exports = roleCartographer;
