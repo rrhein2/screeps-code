c09847728ea1dacadf51a7012e88fc8d25f6fbeb
Richie Rhein <richardrhein2@gmail.com>
2019-11-14 11:40:45 -0600

    Added spawning of Mark 0-2 for the defender class

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -45,5 +45,37 @@ StructureSpawn.prototype.spawner =
 	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
 	        	}
 	        }
+	        // Spawn cartographer
+	        else if(next.substring(0, 2) == "CT")
+	        {
+	        	if(this.spawnCreep([MOVE], "Cart-"+Game.time, {memory:{role:'cart', home:this.room.name}}) == 0)
+	        	{
+	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        	}
+	        }
+	        // Spawn Defender Mark 0
+	        else if(next.substring(0, 2) == "D0")
+	        {
+	        	if(this.spawnCreep([TOUGH, MOVE, ATTACK], "Prot_Mk.0-"+Game.time, {memory:{role:'prot', selector:-1, home:this.room.name}}) == 0)
+	        	{
+	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        	}
+	        }
+	        // Spawn Defender Mark 1
+	        else if(next.substring(0, 2) == "D1")
+	        {
+	        	if(this.spawnCreep([TOUGH, TOUGH, MOVE, MOVE, ATTACK, ATTACK], "Def_Mk.1-"+Game.time, {memory:{role:'prot', home:this.room.name, mark:'D1', selector:-1}}) == 0)
+	        	{
+	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        	}
+	        }
+	        // Spawn Defender Mark 2
+	        else if(next.substring(0, 2) == "D2")
+	        {
+	        	if(this.spawnCreep([TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK], "Def_Mk.2-"+Game.time, {memory:{role:'prot', home:this.room.name, mark:'D2', selector:-1}}) == 0)
+	        	{
+	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        	}
+	        }
 		}
 	}
