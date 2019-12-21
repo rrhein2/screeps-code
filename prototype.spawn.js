d255b407b508d68cfbe65ab6250ab2e192c182a9
Richie Rhein <richardrhein2@gmail.com>
2019-12-21 03:48:42 -0600

    added code for fixing the multiple spawn bug, also added code that should lets harvesters lead to stationary harvesters automatically based on energy available

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -3,11 +3,12 @@ StructureSpawn.prototype.spawner =
 	{
 		if(this.room.memory.spawnQueue.length > 1)
 		{
+			var body = [];
 			var queue = this.room.memory.spawnQueue;
 	        var next = queue.substring(0, queue.indexOf(','));
 	        if(next.substring(0, 2) == "SH")
 	        {
-	            if(this.spawnCreep([MOVE,MOVE, WORK, WORK, WORK, WORK, WORK, WORK, CARRY], "stHarv"+Game.time, {memory:{role:'stHarv', srcID:next.substring(2), home:this.room.name}}) == 0)
+	            if(this.spawnCreep([MOVE,MOVE, WORK, WORK, WORK, WORK, WORK, WORK, CARRY], "stHarv"+Game.time, {memory:{role:'stHarv', srcID:next.substring(2), home:this.room.name, inQueue:false}}) == 0)
 	            {
 	                // if it was spawned, remove from queue
 	                this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
@@ -15,32 +16,110 @@ StructureSpawn.prototype.spawner =
 	        }
 	        else if(next.substring(0, 2) == 'FE')
 	        {
-	            if(this.spawnCreep([MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], "ferry"+Game.time, {memory:{role:'ferry', contID:next.substring(2), home:this.room.name}}) == 0)
+	        	if(this.room.energyAvailable <= 400)
+	        	{
+	        		body = [MOVE, MOVE, CARRY, CARRY];
+	        	}
+	        	else
+	        	{
+	        		for(var i = Math.floor(this.room.energyAvailable/200); i > 0; i--)
+	        		{
+	        			body.push(MOVE);
+	        			body.push(MOVE);
+	        			body.push(CARRY);
+	        			body.push(CARRY);
+	        		}
+	        	}
+	            if(this.spawnCreep(body, "ferry"+Game.time, {memory:{role:'ferry', contID:next.substring(2), home:this.room.name, inQueue:false}}) == 0)
 	            {
 	                // if it was spawned, remove from queue
 	                this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
 	            }
 	        }
 	        // Spawn Harvester Mark 0
-	        else if(next.substring(0,  2) == "H0")
+	        else if(next.substring(0,  2) == "HA")
 	        {
-	        	if(this.spawnCreep([MOVE, WORK, CARRY], "Harv_Mk.0-"+Game.time, {memory:{role:'harvester', home:this.room.name}}) == 0)
+	        	// First, count  the number of Stationary Harvesters in the spawn queue
+	        	var stHarvCount = (this.room.memory.spawnQueue.match(/ a /g) || []).length;
+	        	// Then get the number that exist in the room
+	        	var creeps = this.room.find(FIND_MY_CREEPS)
+	        	for(var i = 0; i < creeps.length; i++)
 	        	{
-	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        		if(creeps[i].role == "stHarv")
+	        		{
+	        			stHarvCount++;
+	        		}
 	        	}
+	        	// if there are less stharvs than sources and you have enoughe nergy to build a stHarv and a ferry
+	        	// then build them
+	        	if(stHarvCount < this.room.find(FIND_SOURCES).length && this.room.energyAvailable >= 1100)
+	        	{
+	        		// Remove the harvester
+	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        		// Add the stharv and ferry
+	        		this.room.memory.spawnQueue = "SH,FE," + this.room.memory.spawnQueue
+	        	}
+	        	// Else just spawn the harvester
+	        	else
+	        	{
+		        	if(this.room.energyAvailable <= 400)
+		        	{
+		        		body = [MOVE, WORK, CARRY];
+		        	}
+		        	else
+		        	{
+		        		for(var i = Math.floor(this.room.energyAvailable/250); i > 0; i--)
+		        		{
+		        			body.push(MOVE);
+		        			body.push(MOVE);
+		        			body.push(WORK);
+		        			body.push(CARRY);
+		        		}
+		        	}
+		        	if(this.spawnCreep(body, "Harv_Mk.0-"+Game.time, {memory:{role:'harvester', home:this.room.name, inQueue:false}}) == 0)
+		        	{
+		        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+		        	}
+		        }
 	        }
 	        // Spawn Builder Mark 0
-	        else if(next.substring(0, 2) == "B0")
+	        else if(next.substring(0, 2) == "BU")
 	        {
-	        	if(this.spawnCreep([MOVE, WORK, CARRY], "Build_Mk.0-"+Game.time, {memory:{role:'builder', home:this.room.name}}) == 0)
+	        	if(this.room.energyAvailable <= 400)
+	        	{
+	        		body.push(MOVE);
+	        		body.push(WORK);
+	        		body.push(CARRY);
+	        	}
+	        	else
+	        	{
+	        		for(var i = Math.floor(this.room.energyAvailable/200); i > 0; i--)
+	        		{
+	        			body.push([MOVE, WORK, CARRY]);
+	        		}
+	        	}
+	        	if(this.spawnCreep(body, "Build_Mk.0-"+Game.time, {memory:{role:'builder', home:this.room.name, inQueue:false}}) == 0)
 	        	{
 	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
 	        	}
 	        }
 	        // Spawn Upgrader Mark 0
-	        else if(next.substring(0, 2) == "U0")
+	        else if(next.substring(0, 2) == "UP")
 	        {
-	        	if(this.spawnCreep([MOVE, WORK, CARRY], "Upgr_Mk.0-"+Game.time, {memory:{role:'upgrader', home:this.room.name}}) == 0)
+	        	if(this.room.energyAvailable <= 400)
+	        	{
+	        		body = [MOVE, WORK, CARRY];
+	        	}
+	        	else
+	        	{
+	        		for(var i = Math.floor(this.room.energyAvailable/200); i > 0; i--)
+	        		{
+	        			body.push(MOVE);
+	        			body.push(WORK);
+	        			body.push(CARRY);
+	        		}
+	        	}
+	        	if(this.spawnCreep(body, "Upgr_Mk.0-"+Game.time, {memory:{role:'upgrader', home:this.room.name, inQueue:false}}) == 0)
 	        	{
 	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
 	        	}
@@ -48,31 +127,29 @@ StructureSpawn.prototype.spawner =
 	        // Spawn cartographer
 	        else if(next.substring(0, 2) == "CT")
 	        {
-	        	if(this.spawnCreep([MOVE], "Cart-"+Game.time, {memory:{role:'cart', home:this.room.name}}) == 0)
+	        	if(this.spawnCreep([MOVE], "Cart-"+Game.time, {memory:{role:'cart', home:this.room.name, inSpawnQueue:false}}) == 0)
 	        	{
 	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
 	        	}
 	        }
 	        // Spawn Defender Mark 0
-	        else if(next.substring(0, 2) == "D0")
+	        else if(next.substring(0, 2) == "DF")
 	        {
-	        	if(this.spawnCreep([TOUGH, MOVE, ATTACK], "Prot_Mk.0-"+Game.time, {memory:{role:'prot', selector:-1, home:this.room.name}}) == 0)
+	        	if(this.room.energyAvailable <= 400)
 	        	{
-	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        		body = [TOUGH, MOVE, MOVE, ATTACK];
 	        	}
-	        }
-	        // Spawn Defender Mark 1
-	        else if(next.substring(0, 2) == "D1")
-	        {
-	        	if(this.spawnCreep([TOUGH, TOUGH, MOVE, MOVE, ATTACK, ATTACK], "Def_Mk.1-"+Game.time, {memory:{role:'prot', home:this.room.name, mark:'D1', selector:-1}}) == 0)
+	        	else
 	        	{
-	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
+	        		for(var i = Math.floor(this.room.energyAvailable/200); i > 0; i--)
+	        		{
+	        			body.push(TOUGH);
+	        			body.push(MOVE);
+	        			body.push(MOVE);
+	        			body.push(ATTACK);
+	        		}
 	        	}
-	        }
-	        // Spawn Defender Mark 2
-	        else if(next.substring(0, 2) == "D2")
-	        {
-	        	if(this.spawnCreep([TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK], "Def_Mk.2-"+Game.time, {memory:{role:'prot', home:this.room.name, mark:'D2', selector:-1}}) == 0)
+	        	if(this.spawnCreep(body, "Prot_Mk.0-"+Game.time, {memory:{role:'prot', selector:-1, home:this.room.name}}) == 0)
 	        	{
 	        		this.room.memory.spawnQueue = queue.substring(queue.indexOf(',')+1);
 	        	}
