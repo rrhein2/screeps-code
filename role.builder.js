b0845b926f97783a977a5cc5c99e2ad10dba8ef2
Richie Rhein <richardrhein2@gmail.com>
2021-11-06 20:40:00 -0500

    Gave access to a repair queue for roads, switched to memory based targeting and improved cpu usage

--- role.builder.js
+++ role.builder.js
@@ -13,6 +13,12 @@ var roleBuilder = {
 		}
 
         if((creep.memory.building && creep.carry.energy == 0) || creep.memory.building == undefined) {
+            // Add reset for target after upgrading here because it wasn't working in the upgrading branch on bottom, this catches the energy == 0 before 
+            //   the branch could
+            if(creep.memory.mode == "u")
+            {
+                creep.memory.target = undefined
+            }
             creep.memory.building = false;
             creep.say('🔄 harvest');
         }
@@ -20,39 +26,144 @@ var roleBuilder = {
             creep.memory.building = true;
             creep.say('🚧 build');
         }
+
         
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            var structures = creep.room.find(FIND_STRUCTURES, {
-                filter: (conts) => {
-                    return (conts.structureType == STRUCTURE_CONTAINER ||
-                            conts.structureType == STRUCTURE_EXTENSION ||
-                            conts.structureType == STRUCTURE_TOWER //||
-                            // conts.structureType == STRUCTURE_ROAD
-                        ) && conts.hits < conts.hitsMax;
-                }
-            });
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-            // If not building, then repairing
-            else if(structures.length)
+        if(creep.memory.building) 
+        {
+            var target
+            if(creep.memory.target == undefined || creep.memory.target == null)
             {
-                if(creep.repair(structures[0]) == ERR_NOT_IN_RANGE)
+                if(creep.room.memory.repairQueue.length > 0)
                 {
-                    creep.travelTo(structures[0]);
+                    target = creep.room.memory.repairQueue.substring(0, queue.indexOf(','))
+                    creep.memory.mode = "r"
+                }
+                else
+                {
+                    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                    // If there are construction sites, then grab one to start building
+                    if(targets.length)
+                    {
+                        target = targets[0].id
+                        creep.memory.mode = "b"
+                    }
+                    // If there are no constructions ites, find a structure to repair
+                    else
+                    {
+                        targets = creep.room.find(FIND_STRUCTURES, {
+                            filter: (conts) => {
+                                return (conts.structureType == STRUCTURE_CONTAINER ||
+                                        conts.structureType == STRUCTURE_EXTENSION ||
+                                        conts.structureType == STRUCTURE_TOWER //||
+                                        // conts.structureType == STRUCTURE_ROAD
+                                    ) && conts.hits < conts.hitsMax;
+                            }
+                        });
+                        // If there are structures to repair, use that as the target
+                        if(targets.length)
+                        {
+                            target = targets[0].id
+                            creep.memory.mode = "r"
+                        }
+                        // IF there are no repairs to be done, upgrade the controller
+                        else
+                        {
+                            target = creep.room.controller.id
+                            creep.memory.mode = "u"
+                        }
+                    }
+                }
+                creep.memory.target = target
+            }
+            else
+            {
+                target = creep.memory.target
+            }
+
+            var mode = creep.memory.mode
+            var targetObj = Game.getObjectById(target)
+            // If the mode is upgrade, move within 3 and upgrade the controller. Once the creep runs out of energy, unassign the target
+            //   to reassess construction sites and repairs
+            if(mode == "u")
+            {
+                if(creep.pos.inRangeTo(targetObj, 3))
+                {
+                    creep.upgradeController(targetObj)
+                }
+                else
+                {
+                    creep.travelTo(targetObj)
                 }
             }
-            // If not building, then upgrading
-            // else if(creep.room.find(FIND_TOMBSTONES).length > 0)
-            // {
-            //     creep.runOtherRole('graverobber')
-            // }
-            else if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            // If the mode is repair or build, move to within 1 of the target, and then either repair or build the target
+            else
+            {
+                // This is needed for the build mode. For some reason, putting the check inside the mode == 'b' case doesn't work
+                if(targetObj == null || targetObj == undefined)
+                {
+                    creep.memory.target = undefined
+                }
+                else
+                {
+                    if(creep.pos.inRangeTo(targetObj, 1))
+                    {
+                        if(mode == 'r')
+                        {
+                            creep.repair(targetObj)
+                            // If the target is repaired, find a new target
+                            if(targetObj.hits == targetObj.hitsMax)
+                            {
+                                creep.memory.target = undefined
+                            }
+                        }
+                        if(mode == 'b')
+                        {
+                            creep.build(targetObj)
+                        }
+                    }
+                    else
+                    {
+                        creep.travelTo(targetObj)
+                    }
+                }
             }
+
+
+
+
+
+            
+            // var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            // var structures = creep.room.find(FIND_STRUCTURES, {
+            //     filter: (conts) => {
+            //         return (conts.structureType == STRUCTURE_CONTAINER ||
+            //                 conts.structureType == STRUCTURE_EXTENSION ||
+            //                 conts.structureType == STRUCTURE_TOWER //||
+            //                 // conts.structureType == STRUCTURE_ROAD
+            //             ) && conts.hits < conts.hitsMax;
+            //     }
+            // });
+            // if(targets.length) {
+            //     if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            //         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            //     }
+            // }
+            // // If not building, then repairing
+            // else if(structures.length)
+            // {
+            //     if(creep.repair(structures[0]) == ERR_NOT_IN_RANGE)
+            //     {
+            //         creep.travelTo(structures[0]);
+            //     }
+            // }
+            // // If not building, then upgrading
+            // // else if(creep.room.find(FIND_TOMBSTONES).length > 0)
+            // // {
+            // //     creep.runOtherRole('graverobber')
+            // // }
+            // else if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            //     creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            // }
         }
         else {
             var energyCont = creep.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -89,6 +200,13 @@ var roleBuilder = {
                 var sources = creep.pos.findClosestByRange(FIND_SOURCES);
                 if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+            // this else is if there are energy stores in the room, then use them
+            else
+            {
+                if(creep.withdraw(energyCont, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(energyCont)
                 }
             }
 
